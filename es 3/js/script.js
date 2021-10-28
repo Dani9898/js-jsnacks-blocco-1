4ce54147ac751155c9fd25e72c674f7cd82bbc58
Dani9898 <danieleoliva98@gmail.com>
2021-10-28 15:13:36 +0200

    es 3 fatto

--- es 3/js/script.js
+++ es 3/js/script.js
@@ -1,1 +1,39 @@
 
+//  Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+// chiedere 10 volte all'utente un numero e salvare ogni numero in un array.
+
+// creo array
+const myArray = [];
+
+
+// chiedere => prompt
+// 10 volte => ciclo che finisce quando myArray.length < 10,
+//  un numero => parseInt(prompt"") 
+// controllare se non è un numero =>  while (isNaN(numero)) numero = parseInt(prompt("Inserisci un numero corretto: ")),
+// aggiungere il numero all'array => .push
+// fare la somma di tutti i numeri dell'array => for( let i = 0...) che aggiunge ad una variabile esterna il valore contenuto nella posizione i dell'array.
+// stampare la somma => log variabile esterna
+
+while (myArray.length < 10) {
+    let num = parseInt(prompt("Inserisci un numero: "));
+    while (isNaN(num)) {
+        num = parseInt(prompt("Inserisci un numero corretto: "));
+   }
+   myArray.push(num);
+}
+
+let somma = 0;
+
+for (let i = 0; i < myArray.length; i++) {
+    somma += myArray[i];
+}
+console.log(somma);
+
+
+
+
+
+
+
+    
