187fffcdfdc16e07827f40ec7719309f044c3583
Dani9898 <danieleoliva98@gmail.com>
2021-10-28 18:42:42 +0200

    es 6 fatto

--- jsnack6/js/script.js
+++ jsnack6/js/script.js
@@ -1,18 +1,35 @@
 // Genera un array di 50 numeri random. Nell’array non devono esserci doppioni.
 
 // genera array di 50 numeri random => for mathrandom
-// se un numero generato è nella lista dell'array allora ricrearlo
+// controllare il numero con tutti gli elementi del vettore, se è uguale ad uno allora ricrealo finche è uguale , altrimenti metterlo
 // aggiungi il numero all'array => .push(n)
 
 const myArray = [];
 
-while (myArray.length < 50) {
-    let numero = Math.floor(Math.random() * 100);
-    while (myArray.includes(numero)) {
-        numero = Math.floor(Math.random() * 100);
+// con includes
+
+// while (myArray.length < 50) {
+//     let numero = Math.floor(Math.random() * 100);
+//     while (myArray.includes(numero)) {
+//         numero = Math.floor(Math.random() * 100);
+//     }
+//     myArray.push(numero); 
+// }
+// console.log(myArray);
+
+while (myArray.length < 10) {
+    let doppione = false;
+    let numero = Math.floor(Math.random() * 10);
+    for (let i = 0; i < myArray.length; i++) {
+        if (numero === myArray[i]) {
+            doppione = true;
+        }
     }
-    myArray.push(numero);
+
+    if (doppione === false) {
+        myArray.push(numero);
+    }
     
 }
-console.log(myArray);
 
+console.log(myArray);
